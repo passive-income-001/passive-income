f116fba1a24c618835fa00fa7fb2640ca0c2c468
squids <sud7555@gmail.com>
2020-12-13 03:54:40 +0530

    initial commit

--- passive_income/settings.py
+++ passive_income/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    
+   
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'passive_income/static')
 ] 
+
+
