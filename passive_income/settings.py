e47a48ef0af664dc5a243fc05662225149b74ff8
squids <sud7555@gmail.com>
2020-12-13 15:46:29 +0530

    first commit

--- passive_income/settings.py
+++ passive_income/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'h0c0gp0kuv%h!+bdz&u359p2g&op-@+oy5@8j=55#2!%p7ivth'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','passivefinance.herokuapp.com']
 
